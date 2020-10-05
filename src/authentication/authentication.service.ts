16b7e8f52fed673323c3ad9593b47c48b76e7bb7
Akos Radler <akos.radler@gmail.com>
2020-10-05 22:04:54 +0200

    Auth

---
+++ src/authentication/authentication.service.ts
@@ -0,0 +1,63 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import RegisterDto from './dto/register.dto';
+import bcrypt from 'bcrypt';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthenticationService {
+  constructor(private readonly userService: UsersService) {}
+
+  public async register(registrationData: RegisterDto) {
+    const hashedPassword = await bcrypt.hash(registrationData.password, 10);
+    try {
+      const createdUser = await this.userService.create({
+        ...registrationData,
+        password: hashedPassword,
+      });
+      createdUser.password = undefined;
+      return createdUser;
+    } catch (error) {
+      if (error?.code === PostgresErrorCode) {
+        throw new HttpException(
+          'The email address is already taken',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      throw new HttpException(
+        'Something went wrong',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  public async getAuthenticatedUser(email: string, plainTextPassword: string) {
+    try {
+      const user = await this.userService.getByEmail(email);
+      await this.verifyPassword(plainTextPassword, user.password);
+      user.password = undefined;
+      return user;
+    } catch (error) {
+      throw new HttpException(
+        'Username or password is incorrect',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  private async verifyPassword(
+    plainTextPassword: string,
+    hashedPassword: string,
+  ) {
+    const isPasswordMatching = await bcrypt.compare(
+      plainTextPassword,
+      hashedPassword,
+    );
+
+    if (!isPasswordMatching) {
+      throw new HttpException(
+        'Username or password is incorrect',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}
