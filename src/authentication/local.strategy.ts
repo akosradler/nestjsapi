f4ca2a2b9730d7f90ce6d6b25dfdc6d61582e92f
Akos Radler <akos.radler@gmail.com>
2020-10-06 23:21:15 +0200

    Autherntication

---
+++ src/authentication/local.strategy.ts
@@ -0,0 +1,14 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import User from 'src/users/users.entity';
+import { AuthenticationService } from './authentication.service';
+
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authenticationService: AuthenticationService) {
+    super({ usernameField: 'email' });
+  }
+
+  async validate(email: string, password: string): Promise<User> {
+    return this.authenticationService.getAuthenticatedUser(email, password);
+  }
+}
